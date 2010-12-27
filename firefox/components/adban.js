ed8e84d4aede71285e6c24ba71fedbe816c9a52f
support <support@ad-ban.appspotmail.com>
2010-12-27 22:41:02 +0000

    updated AdBan server

--- firefox/components/adban.js
+++ firefox/components/adban.js
@@ -335,7 +335,7 @@ adban.prototype = {
       https: true,
       ftp: true,
   },
-  _SERVER_HOST: 'http://localhost:8080',
+  _SERVER_HOST: 'https://dev.latest.ad-ban.appspot.com',
   _ERROR_CODES: {
       NO_ERRORS: 0,
       REQUEST_PARSING_ERROR: 1,
