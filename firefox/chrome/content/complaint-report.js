e90970814235d863cb9f017387cb711edb96a3a2
support <support@ad-ban.appspotmail.com>
2011-01-05 12:25:00 +0000

    removed 'I reloaded the page' checkbox from 'Report ads' dialog, since it can be confusing to users

--- firefox/chrome/content/complaint-report.js
+++ firefox/chrome/content/complaint-report.js
@@ -2,16 +2,6 @@ let $ = function(id) {
   return document.getElementById(id);
 };
 
-let cmdCheckbox = function() {
-  const dialog = $('adban-complaint-dialog');
-  if ($('adban-complaint-checkbox').checked) {
-    dialog.removeAttribute('buttondisabledaccept');
-  }
-  else {
-    dialog.setAttribute('buttondisabledaccept', 'true');
-  }
-};
-
 let complaint_callback = window.arguments[0];
 
 let cmdDialogAccept = function() {
@@ -20,17 +10,12 @@ let cmdDialogAccept = function() {
 
 let init = function() {
   $('adban-complaint-site-url').value = window.arguments[1];
-
-  $('adban-complaint-checkbox').addEventListener('command', cmdCheckbox, false);
   $('adban-complaint-dialog').addEventListener('dialogaccept', cmdDialogAccept, false);
-
   window.addEventListener('unload', shutdown, false);
 };
 
 let shutdown = function() {
-  $('adban-complaint-checkbox').removeEventListener('command', cmdCheckbox, false);
   $('adban-complaint-dialog').removeEventListener('dialogaccept', cmdDialogAccept, false);
-
   window.removeEventListener('load', init, false);
   window.removeEventListener('unload', shutdown, false);
 };
