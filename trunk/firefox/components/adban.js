45b42e3e46b7cd8f1115787a8641c295a69940ef
support@ad-ban.appspotmail.com <support@ad-ban.appspotmail.com@3618e960-0812-b501-58f5-e7071fc0c453>
2011-07-29 07:32:47 +0000

    increased version number

--- trunk/firefox/components/adban.js
+++ trunk/firefox/components/adban.js
@@ -8,7 +8,7 @@ const Cr = Components.results;
 
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 
-const ADDON_VERSION = '1.8.2';
+const ADDON_VERSION = '1.8.3beta1';
 const BACKEND_SERVER_DOMAIN = 'ad-ban.appspot.com';
 const FRONTEND_SERVER_DOMAIN = 'www.advertban.com';
 const BACKEND_SERVER_PROTOCOL = 'https';
