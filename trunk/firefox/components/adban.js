6423688849003471c31952a5f51ade073c85ad70
support@ad-ban.appspotmail.com <support@ad-ban.appspotmail.com@3618e960-0812-b501-58f5-e7071fc0c453>
2011-10-13 00:00:37 +0000

    created v2.2.0

--- trunk/firefox/components/adban.js
+++ trunk/firefox/components/adban.js
@@ -8,7 +8,7 @@ const Cr = Components.results;
 
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 
-const ADDON_VERSION = '2.2.0beta1';
+const ADDON_VERSION = '2.2.0';
 const BACKEND_SERVER_DOMAIN = 'ad-ban.appspot.com';
 const FRONTEND_SERVER_DOMAIN = 'www.advertban.com';
 const BACKEND_SERVER_PROTOCOL = 'https';
